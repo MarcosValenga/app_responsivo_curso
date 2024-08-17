34ad0f2fa670180b14802b211960e52cae8a6b8f
Marcos Valenga <134209298+MarcosValenga@users.noreply.github.com>
2024-08-17 10:15:59 -0400

    final commit

--- lib/pages/home/widgets/course_item.dart
+++ lib/pages/home/widgets/course_item.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class CourseItem extends StatelessWidget {
@@ -6,8 +7,37 @@ class CourseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset('assets/images/banner.jpg'),
+        Image.asset('assets/images/banner.jpg', fit: BoxFit.fitWidth),
+        const SizedBox(height: 4),
+        Flexible(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return const AutoSizeText(
+                'Criação de Apps Android e iOS com flutter - Crie 16 apps',
+                minFontSize: 6,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              );
+            },
+          ),
+        ),
+        const Text(
+          'Aurelion',
+          style: TextStyle(color: Colors.grey),
+        ),
+        const Text(
+          'R\$22,90',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          ),
+        ),
       ],
     );
   }
--- lib/pages/home/widgets/sections/advantages_section.dart
+++ lib/pages/home/widgets/sections/advantages_section.dart
@@ -1,3 +1,4 @@
+import 'package:app_responsivo_curso/breakpoints.dart';
 import 'package:flutter/material.dart';
 
 class AdvantagesSection extends StatelessWidget {
@@ -5,7 +6,7 @@ class AdvantagesSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget buildAdvantage(String title, String subtitle) {
+    Widget buildHorizontalAdvantage(String title, String subtitle) {
       return Row(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -37,17 +38,93 @@ class AdvantagesSection extends StatelessWidget {
       );
     }
 
-    return Container(
-      padding: const EdgeInsets.fromLTRB(10, 0, 16, 16),
-      decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey))),
-      child: Wrap(
-        alignment: WrapAlignment.spaceEvenly,
+    Widget buildVerticalAdvantage(String title, String subtitle) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          buildAdvantage('+45.000 alunos', 'Didática garantida'),
-          buildAdvantage('+45.000 alunos', 'Didática garantida'),
-          buildAdvantage('+45.000 alunos', 'Didática garantida'),
+          const Icon(
+            Icons.star,
+            color: Colors.white,
+            size: 50,
+          ),
+          const SizedBox(width: 8),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            subtitle,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          )
         ],
-      ),
+      );
+    }
+
+    return LayoutBuilder(
+      builder: (_, constraints) {
+        if (constraints.maxWidth >= mobileBreakPoint) {
+          return Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 16, 16),
+            decoration: const BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey))),
+            child: Wrap(
+              alignment: WrapAlignment.spaceEvenly,
+              children: [
+                buildHorizontalAdvantage(
+                  '+45.000 alunos',
+                  'Didática garantida',
+                ),
+                buildHorizontalAdvantage(
+                  '+45.000 alunos',
+                  'Didática garantida',
+                ),
+                buildHorizontalAdvantage(
+                  '+45.000 alunos',
+                  'Didática garantida',
+                ),
+              ],
+            ),
+          );
+        } else {
+          return Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 16, 16),
+            decoration: const BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: buildVerticalAdvantage(
+                    '+45.000 alunos',
+                    'Didática garantida',
+                  ),
+                ),
+                Expanded(
+                  child: buildVerticalAdvantage(
+                    '+45.000 alunos',
+                    'Didática garantida',
+                  ),
+                ),
+                Expanded(
+                  child: buildVerticalAdvantage(
+                    '+45.000 alunos',
+                    'Didática garantida',
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+      },
     );
   }
 }
